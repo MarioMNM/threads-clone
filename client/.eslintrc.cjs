6d982fdbbaabda844364fe905e5fb740f7bbc6a3
MarioMNM <mario.novella@gmail.com>
2023-10-18 20:04:21 +0200

    init vite react app and ts eslint

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json'
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/no-misused-promises': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
+}
---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css'
+
+function App () {
+  return <>Hi Treads</>
+}
+
+export default App
