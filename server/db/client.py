21985072864d37ecfcd059ad8a811f987ba6fbe9
Mario Novella <mario.novella@gmail.com>
2023-10-24 20:13:51 +0200

    mongodb connection

--- server/db/client.py
+++ server/db/client.py
@@ -6,5 +6,5 @@ from pymongo import MongoClient
 load_dotenv()
 
 db_client = MongoClient(
-    f"mongodb+srv://{os.getenv('DB_DEV_USER')}:{os.getenv('DB_DEV_PASSWORD')}@threads-clone.nocwdvz.mongodb.net/?retryWrites=true&w=majority"
+    os.getenv("DB_URL")
 ).dev
